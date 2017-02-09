338dbae37abe8f342e32346ebfd3d4767ed5e3d1
Marc Kessler <marc.kesslernyc@gmail.com>
2017-02-08 17:52:41 -0800

    updated readme and fixed an issue in the code where set and sorted was switched:

--- kyu5/src/csv_columns.py
+++ kyu5/src/csv_columns.py
@@ -2,18 +2,17 @@ csv = 'a,b,c,d,e\n1,2,3,4,5\nf,g,h,i,j'
 indeces = [1, 3, 5, 7]
 
 def csv_columns(csv, indeces):
+    indeces = sorted(set(indeces))
     #split csv into lists
     ld = [l.split(',') for l in csv.splitlines()]
     #generate list of output strings
     out_csv = []
     for l in ld:
-        while True:
-            for i in indeces:
-                if i >= len(ld[0]):
-                    break
-                out_csv.append(l[i])
-                out_csv.append(',')
-            break
+        for i in indeces:
+            if i >= len(ld[0]):
+                break
+            out_csv.append(l[i])
+            out_csv.append(',')
         if len(out_csv):
             out_csv[-1] = '\n'
     if len(out_csv):
