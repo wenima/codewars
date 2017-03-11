0a7aeee93cfc65bcb1fe22d4653af87c6927cc60
Marc Kessler <marc.kesslernyc@gmail.com>
2017-03-10 18:11:34 -0800

    rewrote function to convert decimal to negabinary

--- kyu5/src/base_neg_2.py
+++ kyu5/src/base_neg_2.py
@@ -1,3 +1,13 @@
-def int_to_negabinary(n):
-    bits = 32
-    return bin(n & int("1"*bits, 2))[2:]
+"""Module to solve the code-kata https://www.codewars.com/kata/base-2/train/python."""
+
+
+def to_nega_binary(n):
+    """Return a string representation of the negabinary value of given n."""
+	res = []
+	while n != 0:
+		remainder = abs(n) % 2
+		res.append(str(remainder))
+		if n < 0 and remainder:
+			n -= 1
+		n = int(n / -2)
+	return (''.join(reversed(res)))
