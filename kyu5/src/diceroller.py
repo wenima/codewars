c65a4cda8860a9de18215a81c67a3e57d9498881
Marc Kessler <marc.kesslernyc@gmail.com>
2017-03-12 18:39:36 -0700

    added tests to get sum of modifiers, added function to pass tests

--- kyu5/src/diceroller.py
+++ kyu5/src/diceroller.py
@@ -2,6 +2,7 @@
 """
 
 import re
+from random import random
 
 
 def validate_input(s):
@@ -15,14 +16,20 @@ def validate_input(s):
     m = re.match(pattern, s)
     return True if m else False
 
+def get_modifiers(s):
+    """Return a list of modifiers of the given dice notation."""
+    pattern = r'[+-]+[1-9][0-9]*'
+    return sum([int(match) for match in re.findall(pattern, s)])
 
-def roll(output='summed'):
-    """Roll Dice defined in input string and apply modifieres if present.
 
-    Keyword arguments:
-    output -- takes either 'summed' or 'verbose' (default: 'summed')
-    'summed' will sum up the roll plus modifieres and return an integers
-    'verbose' returns an object with a list containing all rolls and the sum of
-    all modifiers. If no modifiers are given, returns zero
-    """
-    pass
+# def roll(s, output='summed'):
+#     """Roll Dice defined in input string and apply modifieres if present.
+#
+#     Keyword arguments:
+#     output -- takes either 'summed' or 'verbose' (default: 'summed')
+#     'summed' will sum up the roll plus modifieres and return an integers
+#     'verbose' returns an object with a list containing all rolls and the sum of
+#     all modifiers. If no modifiers are given, returns zero
+#     """
+#     d = validate_input(s)
+#     if d:
--- kyu5/src/test_diceroller.py
+++ kyu5/src/test_diceroller.py
@@ -3,7 +3,7 @@
 import pytest
 
 TEST_INPUT = [
-    ('', False)
+    ('', False),
     ({}, False),
     ('abc', False),
     ('2d6+3 abc', False),
@@ -15,9 +15,35 @@ TEST_INPUT = [
     ('3d7 + 3 -2', True),
 ]
 
+TEST_MODIFIERS = [
+    ('d3+4', 4),
+    ('3d7+3-2', 1),
+    ('d6+10-11', -1),
+    ('d6', 0),
+]
 
-@pytest.mark.parametrize('d, result', TESTS)
+TEST_OUTPUT_SUMMED = [
+    ('d6', 0, 7),
+    ('2d4', 1, 9),
+    ('d3 + 4', 4, 8),
+    ('3d7 + 3 - 2', 3, 23),
+]
+
+
+@pytest.mark.parametrize('d, result', TEST_INPUT)
 def test_dice_notation_input(d, result):
     """Test that dice notation is correct."""
     from diceroller import validate_input
     assert validate_input(d) == result
+
+@pytest.mark.parametrize('d, result', TEST_MODIFIERS)
+def test_get_modifiers(d, result):
+    """Test modifiers are correctly returned as an integer."""
+    from diceroller import get_modifiers
+    assert get_modifiers(d) == result
+
+# @pytest.mark.parametrize('d, lower, upper', TEST_OUTPUT_SUMMED)
+# def test_dice_roll_summed(d, lower, upper):
+#     """Test that output with summed returns a number within bounds."""
+#     from diceroller import roll
+#     assert lower < roll(d) < upper
