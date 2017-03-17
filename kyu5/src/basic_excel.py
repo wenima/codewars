779647afb952f0b7e08af15f3a30f966e7b4be6e
Marc Kessler <marc.kesslernyc@gmail.com>
2017-03-16 17:20:15 -0700

    separate out parsing of input, using dispatch/command pattern for calling compare functions instead of using eval

--- kyu5/src/basic_excel.py
+++ kyu5/src/basic_excel.py
@@ -1,11 +1,63 @@
 """Module to solve the code-kata https://www.codewars.com/kata/56055244356dc5c45c00001e.
+Per the description of the kata, we assume that if a string is given and it starts
+with anything but an alphanumeric character, it is an operator.
+
+Since the use of eval is not recommended for unknown input and apparently it's
+really hard to safeguard against malicious code using eval, we implement a
+dispatch/commann pattern.
 """
 
 import re
+from fastnumbers import fast_real
 
-def count_if(values,criteria):
+def is_smaller(a, b):
+  return True if a < b else None
+
+def is_greater(a, b):
+  return True if a > b else None
+
+def is_equal_or_smaller(a, b):
+    return True if a <= b else None
+
+def is_equal_or_greater(a, b):
+    return True if a >= b else None
+
+def is_not_equal(a, b):
+    return True if a != b else None
+
+dispatch = {
+  '<': is_smaller,
+  '>': is_greater,
+  '<=': is_equal_or_smaller,
+  '>=': is_equal_or_greater,
+  '<>': is_not_equal,
+}
+
+def compare(command, a, b):
+    return(dispatch[command](a, b))
+
+
+def parse_input(criteria):
+    """Return operator, comparator if operator exists in input."""
+    try:
+        if criteria[:1].isalnum():
+            return '', criteria
+        else:
+            m = re.match(r'([^\d\w]*)?(\d*\.?\d*)', criteria)
+            operator, comparator = m.groups()
+            return operator, comparator
+    except:
+        return '', criteria
+
+
+def countif(values,criteria):
     """Return number of occurences of criteria in list of values."""
-    pass
+    operator, comparator = parse_input(criteria)
+    if len(operator) > 0:
+        return len([value for value in values if compare(operator, value, fast_real(comparator))])
+    else:
+        return len([value for value in values if value == criteria])
+
 
 def sum_if(values,criteria):
     """Return sum of values based on criteria."""
--- kyu5/src/test_basic_excel.py
+++ kyu5/src/test_basic_excel.py
@@ -6,11 +6,17 @@ import pytest
 #try to use tuples for input var
 COUNTIF_INPUT = [
     ([1, 3, 5, 7, 9], 3, 1),
-    (["John","Steve","John"], "John"), 2),
+    (["John","Steve","John"], "John", 2),
+    ([1, 3, 5, 3],'>=3', 3),
+    ([1.5, 3, 5, 3, 0, -1, -5],'<=1.5', 4),
+    ([1, 3, 5, 3.5],'>3', 2),
+    ([1, 3, 5, 3, 0, -1, -5],'<1', 3),
+    ([1, 3, 5, 3, 0, -1, -5],'<>1', 6)
 ]
 
 SUMIF_INPUT = [
     ([2, 4, 6, -1, 3, 1.5],">0", 16.5),
+    ([1, 3, 5, 3], 3, 6),
 ]
 
 AVERAGEIF_INPUT = [
@@ -18,20 +24,20 @@ AVERAGEIF_INPUT = [
 ]
 
 
-@pytest.mark.parametrize('elements, comparator, result', TEST_INPUT)
-def test_countif(elements, comparator, result):
+@pytest.mark.parametrize('values, criteria, result', COUNTIF_INPUT)
+def test_countif(values, criteria, result):
     """Test function returns number of occurences of comparator in elements."""
     from basic_excel import countif
-    assert countif(elements, comparator) == result
+    assert countif(values, criteria) == result
 
-@pytest.mark.parametrize('elements, comparator, result', TEST_INPUT)
-def test_sumif(elements, comparator, result):
-    """Test function returns sum of elements based on comparator."""
-    from basic_excel import sumif
-    assert sumif(elements, comparator) == result
-
-@pytest.mark.parametrize('elements, comparator, result', TEST_INPUT)
-def test_countif(elements, comparator, result):
-    """Test function returns average of elements based on comparator."""
-    from basic_excel import averageif
-    assert averageif(elements, comparator) == result
+# @pytest.mark.parametrize('elements, comparator, result', TEST_INPUT)
+# def test_sumif(elements, comparator, result):
+#     """Test function returns sum of elements based on comparator."""
+#     from basic_excel import sumif
+#     assert sumif(elements, comparator) == result
+#
+# @pytest.mark.parametrize('elements, comparator, result', TEST_INPUT)
+# def test_countif(elements, comparator, result):
+#     """Test function returns average of elements based on comparator."""
+#     from basic_excel import averageif
+#     assert averageif(elements, comparator) == result
