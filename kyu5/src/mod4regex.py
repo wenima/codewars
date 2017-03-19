3827cd5bc2d52ef12945e47ffcb26f5ceab28217
Marc Kessler <marc.kesslernyc@gmail.com>
2017-03-18 17:01:42 -0700

    scaffold commit for kata https://www.codewars.com/kata/mod4-regex

---
+++ kyu5/src/mod4regex.py
@@ -0,0 +1,9 @@
+"""Module to solve the code-kata https://www.codewars.com/kata/mod4-regex.
+"""
+
+import re
+
+
+def mod4(s):
+    """Return True if given s contains a number divisible by 4."""
+    pass
---
+++ kyu5/src/test_mod4regex.py
@@ -0,0 +1,23 @@
+"""Tests for codewars kata https://www.codewars.com/kata/mod4-regex."""
+
+import pytest
+
+
+TEST_INPUT = [
+    ('[+05620]', True),
+    ('[+05621]', False),
+    ('[-55622]', False),
+    ('[005623]', False),
+    ('[005624]', True),
+    ('[-05628]', True),
+    ('the beginning [0] ... [invalid] numb[3]rs ... the end', True),
+    ("No, [2014] isn't a multiple of 4...", False)
+    ('...may be [+002016] will be.', True)
+]
+
+
+@pytest.mark.parametrize('s, result', TEST_INPUT)
+def test_mod4regex(s, result):
+    """Test mod4-regex returns mod4 correctly."""
+    from mod4regex import mod4
+    assert mod4(s) == result
