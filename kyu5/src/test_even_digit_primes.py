e1054f8e9f0c9247d5737dab41033fb91378e88f
MarcMBPDev <marc.wenicker@gmail.com>
2017-04-01 16:08:48 -0700

    scaffold commit for kata https://www.codewars.com/kata/primes-with-even-digits

---
+++ kyu5/src/test_even_digit_primes.py
@@ -0,0 +1,34 @@
+"""Test for https://www.codewars.com/kata/primes-with-even-digits."""
+
+import pytest
+
+
+TEST_FIB = [
+    (1, 1),
+    (3, 2),
+    (20, 6765),
+    (50, 12586269025),
+    (99, 218922995834555169026),
+]
+
+TEST_DIGITS = [
+(1000, 887),
+(1210, 1201),
+(10000, 8887),
+(500, 487),
+(487, 467),
+]
+
+
+@pytest.mark.parametrize('n, result', TEST_FIB)
+def test_generate_fib(n, result):
+    """Test generate_fib returns correct Fibonacci numbers."""
+    from fib_digit_occurence import generate_fib
+    assert generate_fib(n) == result
+
+
+@pytest.mark.parametrize('n, result', TEST_DIGITS)
+def test_fib_digits(n, result):
+    """Test fib_digits returns correct integer pairs."""
+    from even_digit_primes import f
+    assert f(n) == result
