5072ae014c0fda9c6519a65c75c77c3a3c423a3b
MarcMBPDev <marc.wenicker@gmail.com>
2017-04-15 15:56:37 -0700

    passing tests for one stack for one type of parenthesis

--- kyu5/python/src/test_valid_braces.py
+++ kyu5/python/src/test_valid_braces.py
@@ -3,17 +3,30 @@
 import pytest
 
 
+TEST_PS = [
+    (')))(((', False),
+    ('((()))(', False),
+    ('((()))', True),
+    ('()()()()()()', True),
+    ('())))(((()', False),
+    ('()()())()()()', False),
+    ('(()(()(', False),
+    ('(((((())))', False),
+    ('())(((((', False)
+]
+
 TEST_INPUT = [
     ('(){}[]', True),
     ('(}', False),
-    ('[(])', True),
+    ('[(])', False),
+    ('([{}])', True)
 ]
 
-
-def test_valid_braces_parantheses():
+@pytest.mark.parametrize('s, result', TEST_PS)
+def test_valid_braces_parantheses(s, result):
     """Test valid braces for just parantheses."""
     from valid_braces import valid_braces
-    assert valid_braces('((()))') == True
+    assert valid_braces(s) == result
 
 @pytest.mark.parametrize('s, result', TEST_INPUT)
 def test_valid_braces(s, result):
--- kyu5/python/src/valid_braces.py
+++ kyu5/python/src/valid_braces.py
@@ -1,1 +1,16 @@
 """Solution for kata https://www.codewars.com/kata/valid-braces."""
+
+import queue as q
+
+
+def valid_braces(s):
+    para_stack = q.LifoQueue(maxsize=len(s))
+    for p in s:
+        if p == '(':
+            para_stack.put(p)
+        else:
+            try:
+                para_stack.get_nowait()
+            except q.Empty:
+                return False
+    return para_stack.empty()
