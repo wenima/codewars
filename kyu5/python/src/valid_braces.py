ae41d66f3f3aff3bba7cb3e58305cb10c9bcb7f6
MarcMBPDev <marc.wenicker@gmail.com>
2017-04-15 22:50:50 -0700

    added 4th stack to keep track of last update; passing all basic tests

--- kyu5/python/src/valid_braces.py
+++ kyu5/python/src/valid_braces.py
@@ -4,13 +4,35 @@ import queue as q
 
 
 def valid_braces(s):
-    para_stack = q.LifoQueue(maxsize=len(s))
+    """Return True if given string consisting of a mix of parens, brackets and
+    braces is valid."""
+    parens = q.LifoQueue(maxsize=len(s))
+    braces = q.LifoQueue(maxsize=len(s))
+    brackets = q.LifoQueue(maxsize=len(s))
+    last_updated = q.LifoQueue(maxsize=len(s))
     for p in s:
+#         Tracer()()
         if p == '(':
-            para_stack.put(p)
+            parens.put(p)
+            last_updated.put(parens)
+        elif p == '{':
+            braces.put(p)
+            last_updated.put(braces)
+        elif p == '[':
+            brackets.put(p)
+            last_updated.put(brackets)
         else:
             try:
-                para_stack.get_nowait()
+                if p == ')' and last_updated.get_nowait() == parens:
+                    parens.get_nowait()
+                elif p == '}' and last_updated.get_nowait() == braces:
+                    braces.get_nowait()
+                elif p == ']' and last_updated.get_nowait() == brackets:
+                    brackets.get_nowait()
+                else:
+                    return False
             except q.Empty:
                 return False
-    return para_stack.empty()
+    if parens.empty() and braces.empty() and brackets.empty():
+        return True
+    return False
