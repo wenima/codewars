022ed87b7bdc3712c2ffd34043e2893c1584bac5
MarcMBPDev <marc.wenicker@gmail.com>
2017-04-16 20:15:38 -0700

    passing all basic tests

--- kyu4/src/next_bigger.py
+++ kyu4/src/next_bigger.py
@@ -1,12 +1,20 @@
 """Solution for kata https://www.codewars.com/kata/next-bigger-number-with-the-same-digits."""
 
+from itertools import permutations
 
 def next_bigger(n):
     """Takes a positive integer and returns the next bigger number formed using
     the same digits. If no bigger number can be formed, return -1."""
-    pass
+    if is_biggest(n):
+        return -1
+    sn = str(n)
+    for pn in permutations(sn, len(sn)):
+        num_pn = int(''.join([d for d in pn]))
+        if num_pn > n:
+            return num_pn
+            
 
 def is_biggest(n):
     """Return True if given n is already the biggest number possible to be formed
     of n's digits."""
-    return -1 if int(''.join(sorted([d for d in str(n)], reverse=True))) == n else 0
+    return True if int(''.join(sorted([d for d in str(n)], reverse=True))) == n else False
--- kyu4/src/test_next_bigger.py
+++ kyu4/src/test_next_bigger.py
@@ -4,18 +4,29 @@ import pytest
 
 
 TEST_BIGGEST = [
-    (9, -1),
-    (111, -1),
-    (531, -1),
-    (123, 0),
+    (9, True),
+    (111, True),
+    (531, True),
+    (123, False),
+    (9998, True),
+    (54321, True),
+]
+
+TEST_INPUT = [
+    (999, -1),
+    (12, 21),
+    (513, 531),
+    (2017, 2071),
+    (12345, 12354),
+    (54312, 54321),
 ]
 
 
-# @pytest.mark.parametrize('n, result', TEST_PS)
-# def test_next_bigger(n, result):
-#     """Test next_bigger returns correct next bigger number."""
-#     from next_bigger import next_bigger
-#     assert next_bigger(n) == result
+@pytest.mark.parametrize('n, result', TEST_INPUT)
+def test_next_bigger(n, result):
+    """Test next_bigger returns correct next bigger number."""
+    from next_bigger import next_bigger
+    assert next_bigger(n) == result
 
 @pytest.mark.parametrize('n, result', TEST_BIGGEST)
 def test_is_biggest(n, result):
