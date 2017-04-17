cd349d12ae81c0c1283baf66d28c9b1b1477708d
MarcMBPDev <marc.wenicker@gmail.com>
2017-04-16 18:22:35 -0700

    passing tests for checking if given number is already largest to be formed from it's digits

--- kyu4/src/next_bigger.py
+++ kyu4/src/next_bigger.py
@@ -5,3 +5,8 @@ def next_bigger(n):
     """Takes a positive integer and returns the next bigger number formed using
     the same digits. If no bigger number can be formed, return -1."""
     pass
+
+def is_biggest(n):
+    """Return True if given n is already the biggest number possible to be formed
+    of n's digits."""
+    return -1 if int(''.join(sorted([d for d in str(n)], reverse=True))) == n else 0
--- kyu4/src/test_next_bigger.py
+++ kyu4/src/test_next_bigger.py
@@ -3,15 +3,22 @@
 import pytest
 
 
-TEST_INPUT = [
+TEST_BIGGEST = [
     (9, -1),
     (111, -1),
     (531, -1),
+    (123, 0),
 ]
 
 
-@pytest.mark.parametrize('n, result', TEST_PS)
-def test_next_bigger(n, result):
-    """Test next_bigger returns correct next bigger number."""
-    from next_bigger import next_bigger
-    assert next_bigger(n) == result
+# @pytest.mark.parametrize('n, result', TEST_PS)
+# def test_next_bigger(n, result):
+#     """Test next_bigger returns correct next bigger number."""
+#     from next_bigger import next_bigger
+#     assert next_bigger(n) == result
+
+@pytest.mark.parametrize('n, result', TEST_BIGGEST)
+def test_is_biggest(n, result):
+    """Test is_biggest returns -1 ."""
+    from next_bigger import is_biggest
+    assert is_biggest(n) == result
