b7db0b3230a9841066db18f7d5652e6c1a76643c
MarcMBPDev <marc.wenicker@gmail.com>
2017-04-21 22:02:08 -0700

    fill squares_coords with coords as key and square_nr as value to save lookup function later

--- kyu4/src/sudoku_solver.py
+++ kyu4/src/sudoku_solver.py
@@ -11,19 +11,19 @@ def sudoku_solver(m):
     rows_missing = defaultdict(list)
     cols_missing = defaultdict(list)
     squares_missing = defaultdict(list)
-    squares_coords = defaultdict(list)
+    squares_coords = {}
     dicts = rows_missing, cols_missing, squares_missing
     sq_nr = 0
     for row in range(0, square_sides ** 2, square_sides):
         for col in range(0, square_sides ** 2, square_sides):
             sq_nr += 1
             square = [islice(m[i], col, square_sides + col) for i in range(row, row + square_sides)]
-            fill_given_numbers(square, row, col, sq_nr)
+            fill_given_numbers(square, row, col, sq_nr, dicts, squares_coords)
     for d in dicts:
         d = get_missing(d)
-    candidates = {}
-    starting_spots = get_starting_spots(m)
+    starting_spots = get_starting_spots(m, dicts, squares_coords)
     starting_spots.sort(key=itemgetter(2))
+
     for coordinate in starting_spots:
         get_candidates(coordinate, candidates, rows_missing, cols_missing, squares_missing)
     while True:
@@ -40,15 +40,18 @@ def sudoku_solver(m):
     return m
 
 #mapping given numbers to respective row, cols and squares
-def fill_given_numbers(square, row, col, sq_nr):
+def fill_given_numbers(square, row, col, sq_nr, dicts, squares_coords):
     """Fill dicts with given numbers number for a given square."""
+    rm, cm, sm = dicts
     for row_idx, sr in enumerate(square):
         for col_idx, sv in enumerate(sr):
             coord = (row + row_idx, col + col_idx)
-            squares_coords[sq_nr].append(coord)
-            rows_missing[coord[0]].append(sv)
-            cols_missing[coord[1]].append(sv)
-            squares_missing[sq_nr].append(sv)
+            if sv == 0:
+                squares_coords[coord] = sq_nr
+                continue
+            rm[coord[0]].append(sv)
+            cm[coord[1]].append(sv)
+            sm[sq_nr].append(sv)
 
 
 def get_missing(d):
@@ -58,19 +61,9 @@ def get_missing(d):
     return d
 
 
-def get_square_nr(square_coords, row, col):
-    """Return the square number in the Sudoku starting top left being 1 based
-    on given row and column. If row and column cannot be located, return -1."""
-    target = (row, col)
-    for k, v in square_coords.items():
-        for coord in v:
-            if coord == target:
-                return k
-    return -1
-
-
-def get_starting_spots(m, rows_missing, squares_missing):
+def get_starting_spots(m, dicts, squares_coords):
     """Return a sorted list with coordinates as starting point for sudoku solver."""
+    rm, cm, sm = dicts
     starting_spots = []
     row = 0
     col = 0
@@ -79,11 +72,10 @@ def get_starting_spots(m, rows_missing, squares_missing):
     for col in range(9):
         for row in range(9):
             if m[row][col] == 0:
-                square = get_square_nr(row, col) - 1
-                missing_numbers = len(cols_missing[col]) + len(rows_missing[row])
-                + len(squares_missing[1 if square < 1 else square])
+                square = squares_coords[(row, col)] - 1
+                missing_numbers = len(cm[col]) + len(rm[row]) + len(sm[1 if square < 1 else square])
                 starting_spots.append((row, col, missing_numbers))
-    return starting_spots.sort(key=itemgetter(2))
+    return starting_spots
 
 
 def get_candidates(coordinate, candidates, rows_missing, cols_missing, squares_missing):
