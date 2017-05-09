3d708d8fafb26e00102fc2d5aff232f918d70631
MarcMBPDev <marc.wenicker@gmail.com>
2017-05-09 12:16:30 -0700

    initial commit for https://www.codewars.com/kata/valid-parentheses/python

---
+++ kyu5/python/src/test_valid_parentheses.py
@@ -0,0 +1,23 @@
+"""Tests for codewars kata https://www.codewars.com/kata/valid-parentheses/python."""
+
+import pytest
+
+
+TEST_PS = [
+    (')))(((', False),
+    ('((()))(', False),
+    ('((()))', True),
+    ('()()()()()()', True),
+    ('())))(((()', False),
+    ('()()())()()()', False),
+    ('(()(()(', False),
+    ('(((((())))', False),
+    ('())(((((', False),
+]
+
+
+@pytest.mark.parametrize('s, result', TEST_PS)
+def test_validParentheses(s, result):
+    """Test valid braces for parantheses."""
+    from valid_parentheses import validParentheses
+    assert validParentheses(s) == result
---
+++ kyu5/python/src/valid_parentheses.py
@@ -0,0 +1,7 @@
+"""Solution for kata https://www.codewars.com/kata/valid-parentheses/python.
+Please note that function names sometimes do not follow the suggested convention of
+snake_case due to the kata's author not following it."""
+
+def validParentheses(s):
+    """Return a boolean if given string has valid parentheses."""
+    pass
