8639895944fe98a41799df90c145790e4c964dac
MarcMBPDev <marc.wenicker@gmail.com>
2017-05-09 12:32:42 -0700

    add continue if char is not a parentheses

--- kyu5/python/src/valid_parentheses.py
+++ kyu5/python/src/valid_parentheses.py
@@ -4,4 +4,15 @@ snake_case due to the kata's author not following it."""
 
 def validParentheses(s):
     """Return a boolean if given string has valid parentheses."""
-    pass
+    stack = []
+    for c in s:
+        if c not in ['(', ')']:
+            continue
+        if c == '(':
+            stack.append(c)
+        else:
+            try:
+                stack.pop()
+            except IndexError:
+                return False
+    return len(stack) == 0
