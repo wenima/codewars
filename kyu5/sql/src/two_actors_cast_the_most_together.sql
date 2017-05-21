2f88ba1671358940973b0530ac1a31e79b05d90b
MarcMBPDev <marc.wenicker@gmail.com>
2017-05-21 15:58:01 -0700

    joining in film table to get all titles of films co-starred

--- kyu5/sql/src/two_actors_cast_the_most_together.sql
+++ kyu5/sql/src/two_actors_cast_the_most_together.sql
@@ -27,3 +27,29 @@ SELECT a1.first_name||' '||a1.last_name AS "first_actor",
 FROM actor a1, actor a2, top_team tt
 WHERE tt.actor1 = a1.actor_id
 AND tt.actor2 = a2.actor_id;
+
+-- join in film table to get all titles
+WITH top_team (actor1, actor2, starts)
+AS
+(
+  select tp.actor_id, fa.actor_id as costar, count(fa.actor_id) as starts
+  from film_actor fa, film_actor tp
+  WHERE fa.film_id = tp.film_id
+  AND fa.actor_id <> tp.actor_id
+  GROUP BY tp.actor_id, fa.actor_id
+  order by starts DESC, tp.actor_id
+  LIMIT 1
+)
+
+SELECT a1.first_name||' '||a1.last_name as "first_actor",
+      a2.first_name||' '||a2.last_name as "second_actor",
+      f.title
+FROM actor a1, actor a2, top_team tt, film f
+WHERE tt.actor1 = a1.actor_id
+AND tt.actor2 = a2.actor_id
+AND f.film_id IN (
+    select fa1.film_id from film_actor fa1, film_actor fa2
+    where fa1.film_id = fa2.film_id
+    and fa1.actor_id = a1.actor_id
+    and fa2.actor_id = a2.actor_id
+                );
