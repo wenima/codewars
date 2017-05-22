fedb25f989e2e84823625bce57e73c5995f0d101
MarcMBPDev <marc.wenicker@gmail.com>
2017-05-22 12:10:06 -0700

    split search into 2 as data was not accounted for

--- kyu4/sql/src/messy_data.sql
+++ kyu4/sql/src/messy_data.sql
@@ -6,7 +6,38 @@ CREATE EXTENSION pg_trgm;
 CREATE INDEX prospects_idx ON prospects USING GIN(full_name gin_trgm_ops);
 
 SELECT distinct c.id, c.first_name, c.last_name, c.credit_limit AS "old_limit",
-                p.credit_limit AS "new_limit"
+                max(p.credit_limit) AS "new_limit"
 FROM customers c, prospects p
 WHERE lower(p.full_name) LIKE '%' || lower(c.first_name) || ' ' || lower(c.last_name) || '%'
 AND p.credit_limit > c.credit_limit;
+
+-- not showing the max values for some cases and missing records
+WITH higher_limits (id, first_name, last_name, old_limit, new_limit)
+AS
+(SELECT distinct c.id, c.first_name, c.last_name, c.credit_limit as "old_limit",
+p.credit_limit as "new_limit"
+FROM customers c, prospects p
+where lower(p.full_name) like '%' || lower(c.first_name) || ' ' || lower(c.last_name) || '%'
+and p.credit_limit > c.credit_limit)
+
+select c.id, c.first_name, c.last_name, h.old_limit, max(h.new_limit) as new_limit
+from higher_limits h, customers c
+where h.id = c.id
+group by c.id, c.first_name, c.last_name, h.old_limit
+order by first_name;
+
+--trying to split the like search in 2
+WITH higher_limits (id, first_name, last_name, old_limit, new_limit)
+AS
+(SELECT distinct c.id, c.first_name, c.last_name, c.credit_limit as "old_limit",
+p.credit_limit as "new_limit"
+FROM customers c, prospects p
+where lower(p.full_name) like '%' || lower(c.first_name) || '%'
+and lower(p.full_name) like '%' || lower(c.last_name) || '%'
+and p.credit_limit > c.credit_limit)
+
+select c.id, c.first_name, c.last_name, h.old_limit, max(h.new_limit) as new_limit
+from higher_limits h, customers c
+where h.id = c.id
+group by c.id, c.first_name, c.last_name, h.old_limit
+order by first_name;
