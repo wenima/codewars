d210b345fdd6b32a350e7c9f4b42199f18289f4a
MarcMBPDev <marc.wenicker@gmail.com>
2017-05-22 12:58:34 -0700

    refactor out with stmt as it's not needed

--- kyu4/sql/src/messy_data.sql
+++ kyu4/sql/src/messy_data.sql
@@ -4,19 +4,14 @@ CREATE EXTENSION pg_trgm;
 
 CREATE INDEX prospects_idx ON prospects USING GIN(full_name gin_trgm_ops);
 
-WITH higher_limits (id, first_name, last_name, old_limit, new_limit)
-AS (
-  SELECT c.id, c.first_name, c.last_name,
-    c.credit_limit AS "old_limit",
-    p.credit_limit AS "new_limit"
-    FROM customers c, prospects p
-    WHERE lower(p.full_name) LIKE '%' || lower(c.first_name) || '%'
-    AND lower(p.full_name) LIKE '%' || lower(c.last_name) || '%'
-    AND p.credit_limit > c.credit_limit
-  )
-
-SELECT c.first_name, c.last_name, h.old_limit, max(h.new_limit) AS new_limit
-FROM higher_limits h, customers c
-WHERE h.id = c.id
-GROUP BY c.first_name, c.last_name, h.old_limit
+SELECT
+  c.first_name,
+  c.last_name,
+  c.credit_limit AS "old_limit",
+  max(p.credit_limit) AS "new_limit"
+FROM customers c, prospects p
+WHERE lower(p.full_name) LIKE '%' || lower(c.first_name) || '%'
+AND lower(p.full_name) LIKE '%' || lower(c.last_name) || '%'
+AND p.credit_limit > c.credit_limit
+GROUP BY c.first_name, c.last_name, c.credit_limit
 ORDER BY first_name;
