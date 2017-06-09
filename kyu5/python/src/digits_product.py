cdc65c902b2aa703742d3907766ac0facde389e6
MarcMBPDev <marc.wenicker@gmail.com>
2017-06-09 10:41:26 -0700

    change funtion from reursive to iterative as base test case is an exception

--- kyu5/python/src/digits_product.py
+++ kyu5/python/src/digits_product.py
@@ -1,18 +1,21 @@
 """Test for https://www.codewars.com/kata/simple-fun-number-81-digits-product/."""
 
-def digits_product(n, out):
+def digits_product(n):
     """Return the smallest integer composed of products of n."""
-    if n == 1 and not out:
-        return 11
-    elif n == 1:
-        if len(out) == 1:
-            return int(''.join(['1'] + sorted(out)))
-        else:
-            return int(''.join(sorted(out)))
+    out = []
+    if n == 1: return 11
     if n == 0: return 10
-    for i in range(9, 0, -1):
-        if i == 1:return -1
-        if n % i:
-            continue
-        out.append(str(i))
-        return digits_product(n//i, out)
+    while True:
+        for i in range(9, 0, -1):
+            if i == 1 and len(out) == 0:
+                return -1
+            if n % i:
+                continue
+            out.append(str(i))
+            n //= i
+            break
+        if n == 1 and len(out) == 1:
+            out.append(str(n))
+            break
+        elif n == 1:break
+    return int(''.join(sorted(out)))
