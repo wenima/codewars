e53019682283da64bc4aec2fbf6ae940a7d5f3c0
MarcMBPDev <marc.wenicker@gmail.com>
2017-06-08 23:14:43 -0700

    add test for kata https://www.codewars.com/kata/simple-fun-number-81-digits-product/

---
+++ kyu5/python/src/test_digits_products.py
@@ -0,0 +1,23 @@
+"""Test for https://www.codewars.com/kata/simple-fun-number-81-digits-product/."""
+
+import pytest
+
+
+TEST_DIGITS = [
+(12, 26),
+(19, -1),
+(450, 2559),
+(0, 10),
+(13, -1),
+(1, 11),
+(5, 15),
+(10, 25),
+]
+
+
+@pytest.mark.parametrize('n, result', TEST_DIGITS)
+def test_digits_product(n, result):
+    """Test fib_digits returns correct integer pairs."""
+    from digits_product import digits_product
+    out = []
+    assert digits_product(n, out) == result
