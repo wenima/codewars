e6da7a69626306aad4a3a48e973e3d70a6182fa9
MarcMBPDev <marc.wenicker@gmail.com>
2017-06-17 22:44:53 -0700

    scaffold commit with base test case

---
+++ kyu5/python/src/find_unique_str.py
@@ -0,0 +1,6 @@
+"""Module to solve the code-kata https://www.codewars.com/kata/find-the-unique-string."""
+
+
+def find_uniq(arr):
+    """Return the unique element from a list of strings."""
+    pass
---
+++ kyu5/python/src/test_find_unique_str.py
@@ -0,0 +1,16 @@
+"""Tests for codewars kata https://www.codewars.com/kata/find-the-unique-string."""
+
+import pytest
+
+
+TEST_INPUT = [
+    ([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ], 'BbBb'),
+    ([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ], 'foo'),
+]
+
+
+@pytest.mark.parametrize('arr, result', TEST)
+def test_find_uniq(arr, result):
+    """Test that function returns unique element."""
+    from find_uniqe_str import find_uniq
+    assert find_uniq(arr) == result
