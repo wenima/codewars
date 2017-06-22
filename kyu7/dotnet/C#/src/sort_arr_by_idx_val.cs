2d18c5d6efa5e16d9ccd782b8b44d6372c1bb821
MarcMBPDev <marc.wenicker@gmail.com>
2017-06-21 17:12:47 -0700

    add other solutions for comparison

--- kyu7/dotnet/C#/src/sort_arr_by_idx_val.cs
+++ kyu7/dotnet/C#/src/sort_arr_by_idx_val.cs
@@ -8,13 +8,15 @@ public class Kata
 {
   public static int[] SortByValueAndIndex(int[] array)
   {
-    array.ToList().ForEach(Console.WriteLine);
+    arr.ToList().ForEach(Console.WriteLine);
+    int i = 1;
     var tuple_lst = new List<Tuple<string, int, int>>();
     foreach (int n in array)
     {
       string t_name = n.ToString();
-      var tuple = new Tuple<string, int, int>(t_name, n, (Array.IndexOf(array, n) + 1) * n);
+      var tuple = new Tuple<string, int, int>(t_name, n, i * n);
       tuple_lst.Add(tuple);
+      i++;
     }
     tuple_lst.Sort((x, y) => x.Item3 - y.Item3);
     var ret_lst = new List<int>();
@@ -26,3 +28,28 @@ public class Kata
     return ret_lst.ToArray();
   }
 }
+
+// highest voted
+  public static int[] SortByValueAndIndex(int[] array)
+  {
+    int i = 1;
+    return array.OrderBy(n => n * i++).ToArray();
+  }
+
+// using a for loop and index
+
+  public static int[] SortByValueAndIndex(int[] array)
+  {
+    int[] keys = new int[array.Length];
+    for (int i = 0; i < array.Length; i++) keys[i] = array[i] * (i + 1);
+    Array.Sort(keys, array);
+    return array;
+  }
+
+// using tuples and Linq
+
+  public static int[] SortByValueAndIndex(int[] array)
+  {
+      var tuples = array.Select((t, i) => new Tuple<int, int>(t, (i + 1) * t)).ToList();
+        return tuples.OrderBy(x => x.Item2).ToList().Select(x => x.Item1).ToArray();
+  }
