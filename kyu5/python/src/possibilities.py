741af34a2947349a070301c14e66ddf6718889ed
MarcMBPDev <marc.wenicker@gmail.com>
2017-06-25 16:03:41 -0700

    pass basic tests

--- kyu5/python/src/possibilities.py
+++ kyu5/python/src/possibilities.py
@@ -1,6 +1,21 @@
 """Solution for codewars kata https://www.codewars.com/kata/1-s-0-s-and-wildcards/train/python."""
 
-def possibilities(param):
+from collections import defaultdict
+
+
+def possibilities(s):
     """Retun a list which contains all possibilites for a given string including
     ? as wildcards, where ? is replaced with 1 and 0."""
-    return param
+    d = defaultdict(list)
+    last = 0
+    for idx, c in enumerate(s):
+        if c == '?':
+            if d:
+                for ss in d[last]:
+                    d[idx].append(ss + s[last + 1:idx] + '0')
+                    d[idx].append(ss + s[last + 1:idx] + '1')
+            else:
+                d[idx].append(s[:idx] + '0')
+                d[idx].append(s[:idx] + '1')
+                last = idx
+    return d[len(s) - 1]
