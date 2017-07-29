8b768613735024dcb29179111793c7c41a858263
Marc KesslerWenicker <marc.wenicker@gmail.com>
2017-07-29 19:30:30 -0400

    initial commit to setup tests

---
+++ kyu4/dotnet/Csharp/PokerRankingsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PokerRankingsSolution
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void TestHandIsAStraight()
+        {
+            var heros_hand = new PokerHand<string> { "2H 3H 4H 5H 6S" };
+        }
+    }
+}
