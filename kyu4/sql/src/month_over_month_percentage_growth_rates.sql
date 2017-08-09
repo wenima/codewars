534aaf26101701c8edf0405608791adf5684ec46
MarcMBPDev <marc.wenicker@gmail.com>
2017-08-08 20:03:36 -0700

    add lag

--- kyu4/sql/src/month_over_month_percentage_growth_rates.sql
+++ kyu4/sql/src/month_over_month_percentage_growth_rates.sql
@@ -1,1 +1,7 @@
 -- Solution to codewars kata https://www.codewars.com/kata/calculating-month-over-month-percentage-growth-rate
+
+WITH cte AS (SELECT created_at::date as date FROM posts)
+
+SELECT date, count(date), lag(count(*), 1) OVER (ORDER BY date)
+FROM cte
+GROUP BY date;
