7b07508f599851b5bbc31409e4df386b2299abee
MarcMBPDev <marc.wenicker@gmail.com>
2017-08-09 06:06:37 -0700

    add percent_growth, monthly buckets

--- kyu4/sql/src/month_over_month_percentage_growth_rates.sql
+++ kyu4/sql/src/month_over_month_percentage_growth_rates.sql
@@ -2,6 +2,7 @@
 
 WITH cte AS (SELECT created_at::date as date FROM posts)
 
-SELECT date, count(date), lag(count(*), 1) OVER (ORDER BY date)
+SELECT date, count(date),
+100 * (count(*) - lag(count(*), 1) OVER (ORDER BY date)) / lag(count(*), 1) OVER (ORDER BY date) || '%' as percent_growth
 FROM cte
 GROUP BY date;
