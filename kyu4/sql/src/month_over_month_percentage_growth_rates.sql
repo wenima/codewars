36d36e156f84fc49701f72b2b3eb9991ccd39226
MarcMBPDev <marc.wenicker@gmail.com>
2017-08-10 07:00:56 -0700

    passing all test

--- kyu4/sql/src/month_over_month_percentage_growth_rates.sql
+++ kyu4/sql/src/month_over_month_percentage_growth_rates.sql
@@ -1,8 +1,9 @@
 -- Solution to codewars kata https://www.codewars.com/kata/calculating-month-over-month-percentage-growth-rate
+-- SQL Flavor: Postgres 9.6
 
-WITH cte AS (SELECT created_at::date as date FROM posts)
+WITH cte AS (SELECT date_trunc('month', created_at)::date as date FROM posts)
 
 SELECT date, count(date),
-100 * (count(*) - lag(count(*), 1) OVER (ORDER BY date)) / lag(count(*), 1) OVER (ORDER BY date) || '%' as percent_growth
+ROUND(100 * (count(*) - lag(count(*), 1) OVER (ORDER BY date)) / lag(count(*), 1) OVER (ORDER BY date)::numeric,1) || '%' as percent_growth
 FROM cte
 GROUP BY date;
