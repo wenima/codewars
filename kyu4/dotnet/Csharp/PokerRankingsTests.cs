95c2dfd958b6c7df5c69969b66bd3f7cdc6846fe
Marc KesslerWenicker <marc.wenicker@gmail.com>
2017-08-19 19:50:16 -0400

    add tests for flush, straight, highcard - again after saving the file :)

--- kyu4/dotnet/Csharp/PokerRankingsTests.cs
+++ kyu4/dotnet/Csharp/PokerRankingsTests.cs
@@ -13,8 +13,32 @@ namespace PokerRankingsSolution
         [Test]
         public void TestHandIsAStraight()
         {
-            var heros_hand = new PokerHand("2H 3H 4H 5H 6S");
-            Assert.That(delegate { PokerHand.IsStraight(); }, Is.EqualTo(true));
+            var HeroesHand = new PokerHand("2H 3H 4H 5H 6S");
+            Assert.That(delegate { HeroesHand.IsStraight(); }, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void TestHandIsAFlush()
+        {
+            var HeroesHand = new PokerHand("AS 3S 4S 8S 2S");
+            Assert.That(delegate { HeroesHand.IsFlush(); }, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void TestHandHasCorrectHighCard()
+        {
+            var HeroesHand = new PokerHand("8H 9H QS JS KH");
+            HeroesHand.setHighCards();
+            Assert.That(delegate { HeroesHand.HighCards.pop()[0]; }, Is.EqualTo("K"));
+            Assert.That(delegate { HeroesHand.HighCards.pop()[0]; }, Is.EqualTo("Q"));
+        }
+
+        [Test]
+        public void TestCompareHeroToVillain()
+        {
+            var HeoresHand = new PokerHand("2H 3H 4H 5H 6H");
+            var VillainsHand = new PokerHand("KS AS TS QS JS");
+
         }
     }
 }
