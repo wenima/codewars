c80ce28fb226bdc30cd452d6200f26504f62fb58
Marc KesslerWenicker <marc.wenicker@gmail.com>
2017-08-20 22:16:23 -0400

    add class PokerHand and TupleList

---
+++ kyu4/dotnet/Csharp/PokerHand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokerRankingsSolution
+{
+    class PokerHand
+    {
+        private TupleList<string, int> HighCards { get; set; }
+
+    }
+}
