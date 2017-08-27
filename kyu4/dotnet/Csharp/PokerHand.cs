5eac1a300a86fea11045c6f3419fb43896e037d2
Marc KesslerWenicker <marc.wenicker@gmail.com>
2017-08-27 19:45:30 -0400

    add readonly dict holding card values

--- kyu4/dotnet/Csharp/PokerHand.cs
+++ kyu4/dotnet/Csharp/PokerHand.cs
@@ -4,9 +4,37 @@ using System.Text;
 
 namespace PokerRankingsSolution
 {
+
+    class Constants
+    {
+        private readonly Dictionary<string, Tuple<string, int>> ranks = new Dictionary<string, Tuple<string, int>>()
+        {
+            { "A", new Tuple<string, int>("Ace", 14) },
+            { "K", new Tuple<string, int>("King", 13) },
+            { "Q", new Tuple<string, int>("Queen", 12) },
+            { "J", new Tuple<string, int>("Jack", 11) },
+            { "T", new Tuple<string, int>("Ten", 10) },
+            { "9", new Tuple<string, int>("Nine", 9) },
+            { "8", new Tuple<string, int>("Eight", 8) },
+            { "7", new Tuple<string, int>("Seven", 7) },
+            { "6", new Tuple<string, int>("Six", 6) },
+            { "5", new Tuple<string, int>("Five", 5) },
+            { "4", new Tuple<string, int>("Four", 4) },
+            { "3", new Tuple<string, int>("Three", 3) },
+            { "2", new Tuple<string, int>("Two", 2) },
+        };
+    }
     class PokerHand
     {
+        public string hand { get; set; }
         private TupleList<string, int> HighCards { get; set; }
+        public string[] vals { get; set; }
+
+        public PokerHand(string hand)
+        {
+            hand = hand.Trim();
+            HighCards = new TupleList<string, int>();
+        }
 
     }
 }
