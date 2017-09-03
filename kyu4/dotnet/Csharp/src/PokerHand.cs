91d0f8b2ca09d5a963031d01447d514ea029d06f
MarcMBPDev <marc.wenicker@gmail.com>
2017-09-03 11:24:31 -0700

    fix build issues

--- kyu4/dotnet/Csharp/src/PokerHand.cs
+++ kyu4/dotnet/Csharp/src/PokerHand.cs
@@ -7,30 +7,30 @@ namespace PokerRankingsSolution
 
     class Constants
     {
-        private readonly Dictionary<string, Tuple<string, int>> Ranks = new Dictionary<string, Tuple<string, int>>()
+        public static readonly Dictionary<char, Tuple<string, int>> Ranks = new Dictionary<char, Tuple<string, int>>()
         {
-            { "A", new Tuple<string, int>("Ace", 14) },
-            { "K", new Tuple<string, int>("King", 13) },
-            { "Q", new Tuple<string, int>("Queen", 12) },
-            { "J", new Tuple<string, int>("Jack", 11) },
-            { "T", new Tuple<string, int>("Ten", 10) },
-            { "9", new Tuple<string, int>("Nine", 9) },
-            { "8", new Tuple<string, int>("Eight", 8) },
-            { "7", new Tuple<string, int>("Seven", 7) },
-            { "6", new Tuple<string, int>("Six", 6) },
-            { "5", new Tuple<string, int>("Five", 5) },
-            { "4", new Tuple<string, int>("Four", 4) },
-            { "3", new Tuple<string, int>("Three", 3) },
-            { "2", new Tuple<string, int>("Two", 2) },
+            { 'A', new Tuple<string, int>("Ace", 14) },
+            { 'K', new Tuple<string, int>("King", 13) },
+            { 'Q', new Tuple<string, int>("Queen", 12) },
+            { 'J', new Tuple<string, int>("Jack", 11) },
+            { 'T', new Tuple<string, int>("Ten", 10) },
+            { '9', new Tuple<string, int>("Nine", 9) },
+            { '8', new Tuple<string, int>("Eight", 8) },
+            { '7', new Tuple<string, int>("Seven", 7) },
+            { '6', new Tuple<string, int>("Six", 6) },
+            { '5', new Tuple<string, int>("Five", 5) },
+            { '4', new Tuple<string, int>("Four", 4) },
+            { '3', new Tuple<string, int>("Three", 3) },
+            { '2', new Tuple<string, int>("Two", 2) },
         };
-        private readonly Dictionary<string, Tuple<string, int>> Suits = new Dictionary<string, Tuple<string, int>>()
+        public static readonly Dictionary<char, Tuple<string, int>> Suits = new Dictionary<char, Tuple<string, int>>()
         {
-            { "S": ('Spades', 1) },
-            { "H": ('Hearts', 1) },
-            { "D": ('Diamonds', 1) },
-            { "C": ('Clubs') }
+            { 'S', new Tuple<string, int>("Spades", 1) },
+            { 'H', new Tuple<string, int>("Hearts", 1) },
+            { 'D', new Tuple<string, int>("Diamonds", 1) },
+            { 'C', new Tuple<string, int>("Clubs", 1) }
         };
-        private readonly Dictionary<string, int> MadeHands = new Dictionary<string, int>()
+        public static readonly Dictionary<string, int> MadeHands = new Dictionary<string, int>()
         {
             { "Straight Flush", 9 },
             { "4 of a Kind", 8 },
@@ -45,11 +45,11 @@ namespace PokerRankingsSolution
     }
     class PokerHand
     {
-        public TupleList<string, int> hand { get; set; }
+        public TupleList<string, int> hand { get; set; } = new TupleList<string, int>();
         public TupleList<string, int> HighCards { get; set; }
-        public string[] vals { get; set; }
-        public string[] suits {get; set; }
-        public Dictionary<string, int> val_cnt { get; set; }
+        public List<char> vals { get; set; } = new List<char>();
+        public List<char> suits {get; set; } = new List<char>();
+        public Dictionary<char, int> val_cnt { get; set; }
         public List<int> twopair { get; set; }
         public int handvalue { get; set; }
 
@@ -59,28 +59,24 @@ namespace PokerRankingsSolution
             this.HighCards = new TupleList<string, int>();
             foreach (var card in hand)
             {
-                if (Ranks.ContainsKey(card))
+                if (Constants.Ranks.ContainsKey(card))
                     {
                         this.vals.Add(card);
-                        this.hand.Add(card, Ranks[card][1])
+                        this.hand.Add(card.ToString(), Constants.Ranks[card].Item2);
                     }
             }
-            foreach (var card in Hand)
+            foreach (var card in hand)
             {
-                if (Suits.ContainsKey(card))
+                if (Constants.Suits.ContainsKey(card))
                 {
-                    this.suits.Add(Suits[card])
+                    suits.Add(card);
                 }
             }
             this.hand.Sort((x, y) => y.Item1.CompareTo(x.Item1));
-            foreach (card in vals)
-            {
-                var groups = vals.GroupBy(c => c )
-                                        .Select(c => new { Vals = c.Key, Count = c.Count() });
-            }
+            var groups = vals.GroupBy(c => c )
+                            .Select(c => new { Vals = c.Key, Count = c.Count() });
             val_cnt = groups.ToDictionary( g => g.Vals, g => g.Count);
             handvalue = 0;
         }
-
     }
 }
