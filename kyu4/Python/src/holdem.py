c5ae171763d89922ab69324517563ddab1c0f7f7
MarcMBPDev <marc.wenicker@gmail.com>
2017-10-10 15:41:52 -0700

    add logic to make _is_straight property work for 7 card hand, i.e. a combination of board and hole cards

--- kyu4/Python/src/holdem.py
+++ kyu4/Python/src/holdem.py
@@ -1,25 +1,47 @@
 """Solution for https://www.codewars.com/kata/texas-holdem-poker/python."""
 
-from collections import Counter
+from collections import Counter, defaultdict
 
 CARDS = list("23456789TJQKA")
+
+
+class Board(object):
+
+    def __init__(self, board):
+        self.board = ''.join(c for c in board)
+        self.values = sorted(map(lambda x: self.CARDS.index(x[0]), board.split()), reverse=True)
 
 
 class PokerHand(object):
 
     def __init__(self, hand):
-        self.values = sorted(map(lambda x: self.CARDS.index(x[0]), hand.split(' ')), reverse=True)
+        self.values = sorted(map(lambda x: CARDS.index(x[0]), hand), reverse=True)
         self.groups = []
         for i in sorted(list(set(Counter(self.values).values())), reverse=True):
             self.groups += sorted([x for x in Counter(self.values).items() if x[1] == i], key=lambda x: x[0], reverse=True)
 
-        self.points = 0.0
+        self.val_cnt = defaultdict(int)
+
+        for card in self.values:
+            self.val_cnt[card] += 1
+
+    @property
+    def _score(self):
+        points = 0.0
         for i in range(0, len(self.groups)):
-            self.points += self.groups[i][1] ** 2
+            points += self.groups[i][1] ** 2
 
-        if PokerHand.is_straight(self.values):
-            self.points += 7
+        if self._is_straight():
+            points += 7
+        return points
 
-    @staticmethod
-    def is_straight(values):
-        return len([x for x in range(0, 4) if values[x] - 1 == values[x + 1]]) == 4
+    @property
+    def _is_straight(self):
+        """Return True if combination of board and hole cards make a straight."""
+        hand = [k for k in self.val_cnt.keys()]
+        for x in range(2):
+            hand = sorted(hand[x:], reverse=True)
+            if sum(hand) == 18: return True
+            if len(hand) < 5: return False
+            if len([i for i in range(4) if hand[i] - 1 == hand[i + 1]]) == 4: return True
+        return False
