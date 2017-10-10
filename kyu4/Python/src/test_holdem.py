8055f9df776f0cb063d8960018dd56e5ff0a8575
MarcMBPDev <marc.wenicker@gmail.com>
2017-10-10 15:41:11 -0700

    add test cases for straight

--- kyu4/Python/src/test_holdem.py
+++ kyu4/Python/src/test_holdem.py
@@ -12,11 +12,13 @@ TEST_STRAIGHT = [
     (['9','8','3','3','4'],['5','6'], False),
     (['9','8','K','Q','4'],['7','6'], False),
     (['2','2','3','3','A'],['5','4'], True),
+    (['2','6','3','7','A'],['5','4'], True),
 ]
 
-@pytest.mark.parametrize('board, hand, result', TEST_STRAIGHT)
-def test_hand_is_straight(board, hand, result):
-    """Test that hand has a made hand value of straight."""
+@pytest.mark.parametrize('board, hole_cards, result', TEST_STRAIGHT)
+def test_hand_is_straight(board, hole_cards, result):
+    """Test that combination of board and hole_cards has a made hand value of straight."""
     from holdem import PokerHand
+    hand = ''.join(board + hole_cards)
     heros_hand = PokerHand(hand)
     assert heros_hand._is_straight == result
