3a0478bb8d92cab3a522805ab3aad9736cad2715
MarcMBPDev <marc.wenicker@gmail.com>
2017-10-15 16:57:04 -0700

    initial algorithm

---
+++ kyu3/JavaScript/src/holdem.js
@@ -0,0 +1,17 @@
+// Poker Hand Evaluator by Pat Wilson ©2012
+hands=["4 of a Kind", "Straight Flush", "Straight", "Flush", "High Card",
+"1 Pair", "2 Pair", "Royal Flush", "3 of a Kind", "Full House", "-Invalid-" ];
+handRanks = [8,9,5,6,1,2,3,10,4,7,0];
+
+function calcIndex(cs,ss) {
+  var v,i,o,s; for (i=-1, v=o=0; i<5; i++, o=Math.pow(2,cs[i]*4)) {v += o*((v/o&15)+1);}
+  if ((v%=15)!=5) {return v-1;} else {s = 1<<cs[0]|1<<cs[1]|1<<cs[2]|1<<cs[3]|1<<cs[4];}
+  v -= ((s/(s&-s) == 31) || (s == 0x403c) ? 3 : 1);
+  return v - (ss[0] == (ss[0]|ss[1]|ss[2]|ss[3]|ss[4])) * ((s == 0x7c00) ? -5 : 1);
+}
+
+function getCardStr(str) {
+  return str.replace(/A/g,"14").replace(/K/g,"13").replace(/Q/g,"12")
+            .replace(/J/g,"11").replace(/♠|♣|♥|♦/g,",");
+}
+
