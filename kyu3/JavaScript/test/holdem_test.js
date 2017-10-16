f889fe2dfe3a6c78f7272c1d56120aff8d195f31
MarcMBPDev <marc.wenicker@gmail.com>
2017-10-16 11:42:16 -0700

    setup testing with mocha and chai

--- kyu3/JavaScript/test/holdem_test.js
+++ kyu3/JavaScript/test/holdem_test.js
@@ -1,6 +1,10 @@
-suite('holdem_tests', function() {
-    
-        test('getCardStr returns cardStr with replaced broadway cards', function() {
-            expect(GetCardStr('A♦A♣5♥5♣K♥Q♥K♦')).to.equal(14,14,5,5,13,12,13);
-        });
+suite('simple_test', function() {
+
+    test('test 1', function() {
+        expect(1).to.equal(1);
     });
+
+    // test('getCardStr returns cardStr with replaced broadway cards', function() {
+    //     expect(GetCardStr('A♦A♣5♥5♣K♥Q♥K♦')).to.equal(14,14,5,5,13,12,13);
+    // });
+});
