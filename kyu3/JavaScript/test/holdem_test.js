50fd41085b23d94542bfdaae6d5e730808492d7a
MarcMBPDev <marc.wenicker@gmail.com>
2017-10-18 11:44:56 -0700

    fix description in getCards test; add test to get suits

--- kyu3/JavaScript/test/holdem_test.js
+++ kyu3/JavaScript/test/holdem_test.js
@@ -1,8 +1,11 @@
 suite('holdem_tests', function() {
 
-    test('getCardStr returns cardStr with replaced broadway cards', function() {
-        expect(getCards('A♦A♣5♥5♣K♥Q♥K♦')).to.equal([ '14', '14', '5', '5', '13' ,'12', '13' ]);
+    test('getCards returns array of card values', function() {
+        expect(getCards('A♦A♣5♥5♣K♥Q♥K♦')).to.deep.equal([ '14', '14', '5', '5', '13' ,'12', '13' ]);
     });
 
+    test('getSuits returns an array of suits', function() {
+        expect(getSuits('A♦A♣5♥5♣K♥Q♥K♦')).to.deep.equal([ '♦', '♣', '♥', '♣', '♥', '♥', '♦' ]);
+    });
 
 });
