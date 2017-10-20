9b124fe283a97b6fc81a2258e5df1a86083cea56
MarcMBPDev <marc.wenicker@gmail.com>
2017-10-19 23:37:10 -0700

    add test for rankhand to correctly return the lookup value for the given board + hole cards

--- kyu3/JavaScript/test/holdem_test.js
+++ kyu3/JavaScript/test/holdem_test.js
@@ -17,4 +17,16 @@ suite('holdem_tests', function() {
         expect(getPokerScore([14,14,5,5,12])).to.equal(976220);
     });
 
+    test('rankHand returns correct index to lookup hand value for 2pair', function(){
+        expect(rankHand('A♦A♣5♥5♣K♥Q♥K♦')).to.equal(6);
+    });
+
+    test('rankHand returns correct index to lookup hand value for 4 of a kind', function(){
+        expect(rankHand('A♦A♣A♥A♠5♣K♥Q♥')).to.equal(0);
+    });
+
+    test('rankHand returns correct index to lookup hand value for a flush', function(){
+        expect(rankHand('A♠K♦J♥5♥10♥Q♥3♥')).to.equal(3);
+    });
+
 });
