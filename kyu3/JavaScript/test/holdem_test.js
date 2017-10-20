08bcddeba77cb23cf5fe12e7c56d519c0d44365c
MarcMBPDev <marc.wenicker@gmail.com>
2017-10-20 12:46:27 -0700

    add tests for ranks

--- kyu3/JavaScript/test/holdem_test.js
+++ kyu3/JavaScript/test/holdem_test.js
@@ -32,26 +32,31 @@ suite('holdem_tests', function() {
     test('hand returns string nothing for high card hand', function() {
         hero = hand(['K♠','A♦'],['J♣','Q♥','9♥','2♥','3♦']);
         expect(hero.type).to.equal('nothing');
+        expect(hero.ranks).to.deep.equal(['A','K','Q','J','9']);
     });
 
     test('hand returns string pair', function() {
         hero = hand(['K♠','Q♦'],['J♣','Q♥','9♥','2♥','3♦']);
         expect(hero.type).to.equal('pair');
+        expect(hero.ranks).to.deep.equal(['Q','K','J','9']);
     });
 
     test('hand returns string two pair', function() {
         hero = hand(['K♠','J♦'],['J♣','K♥','9♥','2♥','3♦']);
         expect(hero.type).to.equal('two pair');
+        expect(hero.ranks).to.deep.equal(['K','J','9']);
     });
 
     test('hand returns string three-of-a-kind', function() {
         hero = hand(['4♠','9♦'],['J♣','Q♥','Q♠','2♥','Q♦']);
         expect(hero.type).to.equal('three-of-a-kind');
+        expect(hero.ranks).to.deep.equal(['Q','J','9']);
     });
 
     test('hand returns string straight-flush', function() {
         hero = hand(['8♠','6♠'],['7♠','5♠','9♠','J♠','10♠']);
         expect(hero.type).to.equal('straight-flush');
+        expect(hero.ranks).to.deep.equal(['J','10','9','8','7']);
     });
     
 });
