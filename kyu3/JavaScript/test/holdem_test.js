4cf46bda5a8a549f6ea81c7a93a55c3da60b84d2
MarcMBPDev <marc.wenicker@gmail.com>
2017-10-20 12:38:07 -0700

    add test for hand function to check if the correct type of hand is returned as a string as described in the kata

--- kyu3/JavaScript/test/holdem_test.js
+++ kyu3/JavaScript/test/holdem_test.js
@@ -29,4 +29,29 @@ suite('holdem_tests', function() {
         expect(rankHand('A♠K♦J♥5♥10♥Q♥3♥')).to.equal(3);
     });
 
+    test('hand returns string nothing for high card hand', function() {
+        hero = hand(['K♠','A♦'],['J♣','Q♥','9♥','2♥','3♦']);
+        expect(hero.type).to.equal('nothing');
+    });
+
+    test('hand returns string pair', function() {
+        hero = hand(['K♠','Q♦'],['J♣','Q♥','9♥','2♥','3♦']);
+        expect(hero.type).to.equal('pair');
+    });
+
+    test('hand returns string two pair', function() {
+        hero = hand(['K♠','J♦'],['J♣','K♥','9♥','2♥','3♦']);
+        expect(hero.type).to.equal('two pair');
+    });
+
+    test('hand returns string three-of-a-kind', function() {
+        hero = hand(['4♠','9♦'],['J♣','Q♥','Q♠','2♥','Q♦']);
+        expect(hero.type).to.equal('three-of-a-kind');
+    });
+
+    test('hand returns string straight-flush', function() {
+        hero = hand(['8♠','6♠'],['7♠','5♠','9♠','J♠','10♠']);
+        expect(hero.type).to.equal('straight-flush');
+    });
+    
 });
