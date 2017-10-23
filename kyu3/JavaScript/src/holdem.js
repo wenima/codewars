4f9bdc3b11721c068ab25cf6bfad2cd61ee3cf59
MarcMBPDev <marc.wenicker@gmail.com>
2017-10-23 13:09:40 -0700

    replace set based approach with filter as test suite for kata includes the card that makes a pair or better for comparison of highcards in case of ties which it shouldn't

--- kyu3/JavaScript/src/holdem.js
+++ kyu3/JavaScript/src/holdem.js
@@ -2,6 +2,21 @@
 hands=["four-of-a-kind", "straight-flush", "straight", "flush", "nothing",
 "pair", "two pair", "royal flush", "three-of-a-kind", "full house", "-Invalid-" ];
 handRanks = [8,9,5,6,1,2,3,10,4,7,0];
+var cardsLookup = {
+  2: '2',
+  3: '3',
+  4: '4',
+  5: '5',
+  6: '6',
+  7: '7',
+  8: '8',
+  9: '9',
+  10: '10',
+  11: 'J',
+  12: 'Q',
+  13: 'K',
+  14: 'A'
+};
 
 function calcIndex(cs,ss) {
   var v,i,o,s; for (i=-1, v=o=0; i<5; i++, o=Math.pow(2,cs[i]*4)) {v += o*((v/o&15)+1);}
@@ -46,8 +61,8 @@ function rankHand(str) {
   suits = getSuits(str);
   var index = 10, winCardIndexes, i ,e, wci;
   for (i=0;i<cards.length;i++) { cards[i]-=0; }
-  for (i=0;i<suits.length;i++) 
-      { suits[i] = Math.pow(2, (suits[i].charCodeAt(0)%9824)); }
+  for (i=0;i<suits.length;i++) { suits[i] = Math.pow(2, (suits[i].charCodeAt(0)%9824)); }
+  // console.log('cards: ' + cards);
   var c = getCombinations(5, cards.length);
   var maxRank = 0, winIndex = 10;
   for (i=0; i < c.length; i++) {
@@ -58,22 +73,55 @@ function rankHand(str) {
            maxRank = handRanks[index];
            winIndex = index; 
            wci = c[i].slice();
+           console.log('cs: ' + cs);
        } else if (handRanks[index] == maxRank) {
            //If by chance we have a tie, find the best one
-           var score1 = getPokerScore(cs);
-           var score2 = getPokerScore([cards[wci[0]],cards[wci[1]],cards[wci[2]],
-                                       cards[wci[3]],cards[wci[4]]]);
-           if (score1 > score2) { wci= c[i].slice(); }
+          var other_hand = [cards[wci[0]],cards[wci[1]],cards[wci[2]], cards[wci[3]],cards[wci[4]]];
+          // console.log('cs: ' + cs);
+          // console.log('other hand: ' + other_hand);
+          var score1 = getPokerScore(cs);
+          var score2 = getPokerScore(other_hand);
+          if (score1 > score2) { wci = c[i].slice(); }
        }
   } 
-  return winIndex;
+  return [ winIndex, cards ];
 }
 
 function hand(holeCards, board) {
   heroesHand = holeCards.concat(board).join('');
-  handIndex = rankHand(heroesHand);
+  rankHandReturn = rankHand(heroesHand);
+  console.log('return: ' + rankHandReturn[1]);
   var out = {};
-  out['type'] = hands[handIndex];
-  out['ranks'] = [];
+  var ranks = [];
+  var pairs = rankHandReturn[1].reduce(function(acc, el, i, arr) {
+    if (arr.indexOf(el) !== i && acc.indexOf(el) < 0) acc.push(el); return acc;
+  }, []);
+  console.log('pairs: ' + pairs);
+  if (rankHandReturn[0] == 6) {
+    console.log('6');
+    var filtered = rankHandReturn[1].filter(function(el) {
+      return el != pairs[0] && el != pairs[1]
+      });
+    console.log('filtered: ' + filtered);
+  };
+  if (rankHandReturn[0] == 5) {
+    console.log('5');
+    var filtered = rankHandReturn[1].filter(function(el) {
+      return el !== rankHandReturn[1].includes(el);
+    });
+  };
+  filtered.sort((a, b) => (a - b)).reverse();
+  pairs.sort((a, b) => (a - b)).reverse();
+  console.log('filtered: ' + filtered);
+  console.log('pairs: ' + pairs);
+  // if (rankHandReturn[0] == 8) { var highCards = hand.slice(0, 3); }
+  // if (rankHandReturn[0] == 5) { var highCards = hand.slice(0, 4); }
+  if (rankHandReturn[0] == 5) { var highCards = pairs.concat(filtered.slice(0, 3)); }
+  if (rankHandReturn[0] == 6) { var highCards = pairs.concat(filtered.slice(0, 1)); }
+  for (var i=0;i<highCards.length;i++) { 
+    ranks.push(cardsLookup[highCards[i]]);
+  }
+  out['type'] = hands[rankHandReturn[0]];
+  out['ranks'] = ranks;
   return out;
 }
