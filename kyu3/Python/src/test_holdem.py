5ddd0c5f8bb4f0566075f3255ead1f75c0e5d175
MarcMBPDev <marc.wenicker@gmail.com>
2017-10-27 11:10:43 -0700

    add test for rank_hand

--- kyu3/Python/src/test_holdem.py
+++ kyu3/Python/src/test_holdem.py
@@ -2,6 +2,12 @@
 
 import pytest
 
+HAND_STRENGTH = [
+    ('A♦A♣5♥5♣K♥Q♥K♦', 6),
+    ('A♦A♣A♥A♠5♣K♥Q♥', 0),
+    ('A♠K♦J♥5♥10♥Q♥3♥', 3),
+]
+
 def test_get_cards():
     """Test that get_cards returns a list of strings representing the numerical value of each card."""
     from holdem import get_cards
@@ -16,3 +22,10 @@ def test_get_pokerscore():
     """Test that getPokerScore returns correct integer score for given hand."""
     from holdem import get_pokerscore
     assert get_pokerscore([14,14,5,5,12]) == 976220
+
+@pytest.mark.parametrize('hand, result', HAND_STRENGTH)
+def test_rank_hand(hand, result):
+    """Test that rank_hand returns the correct index from a list of hand strenght for a given hand."""
+    from holdem import rank_hand
+    assert rank_hand(hand)[0] == result
+    
