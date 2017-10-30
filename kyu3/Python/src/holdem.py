08ad0e114afe2017c1ee41ebbe5416ee539bb760
MarcMBPDev <marc.wenicker@gmail.com>
2017-10-29 22:32:07 -0700

    add logic for rank_hand to pass tests

--- kyu3/Python/src/holdem.py
+++ kyu3/Python/src/holdem.py
@@ -11,6 +11,8 @@ LOOKUP = {
     'J' : 11,
     }
 
+HANDRANKS = [8, 9, 5, 6, 1, 2, 3, 10, 4, 7, 0]
+
 def get_cards(hand):
     """Return a list with string representation of a given 7 card hand."""
     card_str_no_suits = ''.join([c if ord(c) < 128 else '' for c in hand])
@@ -28,16 +30,30 @@ def get_pokerscore(hand):
     a = sorted(hand, key=lambda x: (c[x], x), reverse=True)
     return a[0]<<16|a[1]<<12|a[2]<<8|a[3]<<4|a[4]
 
-# def rank_hand(hand):
-#     """Return a tuple with index representing hand strength and final 5 card hand."""
-#     cards = get_cards(hand)
-#     suits = get_suits(hand)
-#     for i, s in enumerate(suits):
-#         suits[i] = int(pow(2, (ord(s) % 9824)))
-#     hand = [(cards[i], suits[i]) for i in range(7)])
-#     c = combinations(hand, 5)
-#     for combo in c:
-#         index = calc_index(combo)
+def rank_hand(hand):
+    """Return a tuple with index representing hand strength and final 5 card hand."""
+    cards = get_cards(hand)
+    suits = get_suits(hand)
+    for i, s in enumerate(suits):
+        suits[i] = int(pow(2, (ord(s) % 9824)))
+    hand = [(cards[i], suits[i]) for i in range(7)]
+    print(hand)
+    c = combinations(hand, 5)
+    max_rank = 0
+    win_index = 10
+    for combo in c:
+        index = calc_index(combo)
+        if HANDRANKS[index] > max_rank:
+            cs, ss = zip(*combo)
+            max_rank = HANDRANKS[index]
+            win_index = index
+            wci = combo
+            hand = cs
+        # elif HANDRANKS[index] == max_rank:
+        #     cs, ss = zip(*combo)
+        #     score_1 = get_pokerscore(cs)
+        #     score_2 = get_pokerscore()
+    return (win_index, hand)
 
 def calc_index(hand):
     """Return the integer index representing the strenght of the hand for a given 5 card hand and suits."""
