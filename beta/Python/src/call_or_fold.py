e37204188de81f1e60bcd44baefc740042421cdd
MarcMBPDev <marc.wenicker@gmail.com>
2017-11-01 11:36:39 -0700

    add function complete_board; fix issue with passing a range object instead of an integer

--- beta/Python/src/call_or_fold.py
+++ beta/Python/src/call_or_fold.py
@@ -21,7 +21,7 @@ def complete_board(deck, board, hero, villain):
     """Return a 5 card board given a an existing board and hole cards from 2 players."""
     if len(board) == 5: return board #board already has been dealt a river, no more cards to deal
     dead = board + hero + villain #dead cards which cannot be in the deck
-    for card in draw_cards(range(5 - len(board)), deck, dead):
+    for card in draw_cards(5 - len(board), deck, dead):
         board.append(card)
     return board
         
