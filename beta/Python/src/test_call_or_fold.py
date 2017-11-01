5cff2a5d7d3469d638a2bbc3ca0a8a6b3b6913dc
MarcMBPDev <marc.wenicker@gmail.com>
2017-11-01 09:50:43 -0700

    add test for creating a new deck

--- beta/Python/src/test_call_or_fold.py
+++ beta/Python/src/test_call_or_fold.py
@@ -8,5 +8,12 @@ INPUT = [
 @pytest.mark.parametrize('hand, result', INPUT)
 def test_convert_suits(hand, result):
     """Test that convert_suits returns a string with actual suits in place of characters representing the suit."""
-    from call_fold import convert_suits
+    from call_or_fold import convert_suits
     assert convert_suits(hand) == result
+
+def test_new_deck():
+    """Test that a new deck with 52 cards is returned and all suits are correct."""
+    from call_or_fold import new_deck
+    deck = new_deck()
+    for suit in ['C', 'H', 'S', 'D']:
+        assert len([c for c in deck if c[1] == suit]) == 13
