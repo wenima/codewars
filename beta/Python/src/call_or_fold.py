d90e42fc87ba1ff04695a72de646a99912083b39
MarcMBPDev <marc.wenicker@gmail.com>
2017-11-01 09:51:06 -0700

    add function to create new  deck

--- beta/Python/src/call_or_fold.py
+++ beta/Python/src/call_or_fold.py
@@ -1,3 +1,9 @@
+
+def new_deck():
+    """Return a 52 deck of cards as a list."""
+    rs = [rank + suit for rank in "A23456789TJQK" for suit in "CDHS"]
+    return rs
+
 def convert_suits(hand):
     """Return a string with suit symbols in place of character representation for a given hand."""
     return hand.replace('D', '♦').replace('H', '♥').replace('S', '♠').replace('C', '♣').replace(' ', '')
