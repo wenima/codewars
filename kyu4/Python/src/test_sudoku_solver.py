1c069f575a81a11119b8405b9ff41846ea974104
MarcMBPDev <marc.wenicker@gmail.com>
2017-12-03 18:24:07 -0800

    fix failing test case

--- kyu4/Python/src/test_sudoku_solver.py
+++ kyu4/Python/src/test_sudoku_solver.py
@@ -39,15 +39,15 @@ kata4 = [
     ]
 
 medium = [
-    [3,0,5,0,2,0,7,0,1],
-    [0,0,0,8,0,3,0,0,2],
-    [0,6,0,0,0,5,0,0,0],
-    [0,7,1,0,0,0,0,5,0],
-    [0,0,6,0,0,0,9,0,0],
-    [0,9,0,0,0,0,4,2,0],
-    [0,0,0,6,0,0,0,9,0],
-    [9,0,0,5,0,4,0,0,5],
-    [6,0,8,0,3,0,2,0,4]
+    [3, 0, 5, 0, 2, 0, 7, 0, 1],
+    [0, 0, 0, 8, 0, 3, 0, 0, 2],
+    [0, 6, 0, 0, 0, 5, 0, 0, 0],
+    [0, 7, 1, 0, 0, 0, 0, 5, 0],
+    [0, 0, 6, 0, 0, 0, 9, 0, 0],
+    [0, 9, 0, 0, 0, 0, 4, 2, 0],
+    [0, 0, 0, 6, 0, 0, 0, 9, 0],
+    [9, 0, 0, 5, 0, 4, 0, 0, 5],
+    [6, 0, 8, 0, 3, 0, 2, 0, 4]
     ]
 
 
@@ -97,4 +97,4 @@ def test_medium_sudoku_validator(medium_sudoku):
     """Test sudoku_validator returns correct result."""
     from sudoku_validator import Sudoku
     medium_sudoku = Sudoku(medium_sudoku)
-    assert medium_sudoku.is_valid() == True
+    assert medium_sudoku.is_valid() == False
