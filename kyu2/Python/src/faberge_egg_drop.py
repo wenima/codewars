3be8c5ef33bb335e89230a40465846ef2ca0a4c2
MarcMBPDev <marc.wenicker@gmail.com>
2017-12-07 12:24:21 -0800

    solve for 2 eggs

--- kyu2/Python/src/faberge_egg_drop.py
+++ kyu2/Python/src/faberge_egg_drop.py
@@ -1,1 +1,12 @@
 """Solution for https://www.codewars.com/kata/faberge-easter-eggs-crush-test."""
+
+def height(eggs, tries, prev_floor=0):
+    """Return the number of max floors a building can have given a number of tries and a number of eggs."""
+    if not tries or not eggs: return 0
+    count_drops = 0
+    interval = egg1 = tries
+    while not count_drops > tries:
+        count_drops += 1
+        interval -= 1
+        egg1 += interval
+    return egg1 + 1
--- kyu2/Python/src/test_faberge_egg_drop.py
+++ kyu2/Python/src/test_faberge_egg_drop.py
@@ -1,3 +1,15 @@
 """Tests for https://www.codewars.com/kata/faberge-easter-eggs-crush-test."""
 
 import pytest
+
+TEST_EGGS = [
+    (0, 14, 0),
+    (2, 0, 0),
+    (2, 14, 105),
+]
+
+@pytest.mark.parametrize('eggs, tries, result', TEST_EGGS)
+def test_height(eggs, tries, result):
+    """Test that function height returns correct result."""
+    from faberge_egg_drop import height
+    assert height(eggs, tries) == result
